bf03bddf6e89f6fa159512218f1a60c6e0d02180
vann <vann@innerfusion.net>
2009-07-27 11:17:17 -0500

    preset background color and use proper event handlers to trigger the fade out

--- jquery.color_fade.js
+++ jquery.color_fade.js
@@ -23,9 +23,17 @@
     $.fn.color_fade = function(opts) {
       
       opts = $.extend({from:"yellow",to:"white",speed:500}, opts || {}); 
-                 
-      $(this).animate({backgroundColor:opts['from']}, opts['speed']);
-      $(this).animate({backgroundColor:opts['to']}, opts['speed']);
+      
+      $(this).css('background-color', opts['from']);            
+      $(this).animate(
+          { backgroundColor:opts['from'] }, 
+          { duration: opts['speed'], 
+            complete: function(){
+              $(this).animate({backgroundColor: opts['to']}, opts['speed'])
+            } });
+        
     }
+    
+   
 
 })(jQuery);
